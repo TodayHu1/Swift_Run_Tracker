0297f96387955cd10b724dab8aaf92cd6292ddb0
Trent Hand <livefree1983@gmail.com>
2017-03-20 12:24:26 -0500

    set up the initial layout of the app, added all the text fields, buttons, and the pickerView needed.

--- Run Tracker/ViewController.swift
+++ Run Tracker/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//TEXTFIELDS
+    @IBOutlet weak var txtRunName: UITextField!
+    @IBOutlet weak var txtRunDistance: UITextField!
+    @IBOutlet weak var txtRunTime: UITextField!
+    
+//LABELS
+    @IBOutlet weak var lblRunNameDisplay: UILabel!
+    @IBOutlet weak var lblRunDistanceDisplay: UILabel!
+    @IBOutlet weak var lblRunTimeDisplay: UILabel!
+    
+//PICKER
+    @IBOutlet weak var pkrRunPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+//Add & Delete Buttons
+    @IBAction func pressAddButton(_ sender: UIButton) {
+    }
 
-
+    @IBAction func pressDeleteButton(_ sender: UIButton) {
+    }
 }
 
