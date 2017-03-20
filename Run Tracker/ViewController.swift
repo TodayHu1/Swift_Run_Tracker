fe54929cf4eee5fd59c106caaa0df34b0453d4bd
Trent Hand <livefree1983@gmail.com>
2017-03-20 12:42:10 -0500

    Database is set up and the entities are beginning to be connected.

--- Run Tracker/ViewController.swift
+++ Run Tracker/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import Foundation
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
 //TEXTFIELDS
     @IBOutlet weak var txtRunName: UITextField!
     @IBOutlet weak var txtRunDistance: UITextField!
@@ -25,6 +27,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pkrRunPicker.delegate = self
+        pkrRunPicker.dataSource = self
+        
+        txtRunName.delegate = self
+        txtRunDistance.delegate = self
+        txtRunTime.delegate = self
         
     }
 
@@ -32,6 +40,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+//These functions determine the size and behavior of the pickerView
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        <#code#>
+    }
+    
+//textField functions
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        <#code#>
+//        lets us determine what kind of characters are allowed in the fields
+    }
+    
 //Add & Delete Buttons
     @IBAction func pressAddButton(_ sender: UIButton) {
     }
